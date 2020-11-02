dcd6ae207ce5be4736e1b24e524a4c1236f6e0a3
Coder12a <38924418+Coder12a@users.noreply.github.com>
2020-11-02 15:31:39 -0600

    Add: on_hasty_guard func

--- pvp_shields/init.lua
+++ pvp_shields/init.lua
@@ -11,6 +11,7 @@ minetest.register_tool("pvp_shields:shield_steel", {
         sound_play("shield_equip", {
             pos = player:get_pos(),
             gain = 0.5,
+            max_hear_distance = 8,
         }, true)
     end,
 
@@ -18,13 +19,17 @@ minetest.register_tool("pvp_shields:shield_steel", {
         sound_play("shield_dequip", {
             pos = player:get_pos(),
             gain = 0.5,
+            max_hear_distance = 5,
         }, true)
     end,
 
     on_block_damage = function(player, damage)
+        local gain = damage / 60
+        
         sound_play("shield_impact1", {
             pos = player:get_pos(),
-            gain = 0.5,
+            gain = gain,
+            max_hear_distance = 4 + damage,
         }, true)
     end,
 
@@ -32,6 +37,17 @@ minetest.register_tool("pvp_shields:shield_steel", {
         sound_play("shield_guard_break", {
             pos = player:get_pos(),
             gain = 0.8,
+            max_hear_distance = 16,
+        }, true)
+    end,
+
+    on_hasty_guard = function(player, damage)
+        local gain = damage / 50
+        
+        sound_play("shield_hasty_guard", {
+            pos = player:get_pos(),
+            gain = gain,
+            max_hear_distance = 6 + damage,
         }, true)
     end
 })
